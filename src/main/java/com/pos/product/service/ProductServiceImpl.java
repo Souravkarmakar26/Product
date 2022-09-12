6458e83c04ebf57a6582e351b82ca33c83bbd6db
Sourav <sourav.karmakar@sap.com>
2022-09-12 15:09:49 +0530

    spring boot sleuth implementation

--- src/main/java/com/pos/product/service/ProductServiceImpl.java
+++ src/main/java/com/pos/product/service/ProductServiceImpl.java
@@ -2,6 +2,8 @@ package com.pos.product.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +12,14 @@ import com.pos.product.repository.ProductRepository;
 @Service
 public class ProductServiceImpl implements ProductService{
 	
+	private static Logger log = LoggerFactory.getLogger(ProductServiceImpl.class);
+	
 	@Autowired
 	private ProductRepository pr;
 
 	@Override
 	public Product saveProduct(Product product) {
+		log.info("Product - "+product);
 		return pr.save(product);		
 	}
 
